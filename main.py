fd39aed6b6d53e76cf003cd4cf8a980b366b7505
Chirag Madaan <appleswiggy@nitro.localdomain>
2021-11-02 10:57:13 +0530

    add generateSignature

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import useful_methods
+from parameters import KnownParameters
+
+groupParameters = KnownParameters.RFC5114_2_3_256
+
+
 class LinkableSignature:
     def __init__(self, Y0, S, C) -> None:
         self.Y0 = Y0
@@ -6,3 +12,22 @@ class LinkableSignature:
 
     def isLinked(self, S1) -> bool:
         return S1.Y0 == self.Y0
+
+
+def generateSignature(message, publicKeys, privateKey, identity):
+    # this way of generating random int might be wrong
+    r = useful_methods.generateRandomInteger(groupParameters.subgroupSize)
+    c = [-1] * len(publicKeys)
+    b = 0
+
+    for i in range(len(publicKeys)):
+        if i != identity:
+            # this way of generating random int might be wrong
+            c[i] = useful_methods.generateRandomInteger(groupParameters.subgroupSize)
+            b = (b + c[i]) % groupParameters.subgroupSize
+
+    x = publicKeys.copy()
+    x[identity] = groupParameters.generator
+    c[identity] = r
+
+    a = useful_methods.power(x, c)  # this is wrong. Fix it later when you figure out
--- useful_methods.py
+++ useful_methods.py
@@ -1,4 +1,20 @@
+import random
+
+
 def HexToInt(hexa):
     hexa = hexa.replace(" ", "").replace("\n", "")
     integer = int(hexa, 16)
     return integer
+
+
+def generateRandomInteger(max):
+    n = len(str(max)) + 8
+    random_number = random.randint(10 ** n, 10 ** (n + 1) - 1)
+    return random_number % max
+
+
+def power(a, b):
+    product = 1
+    for i, j in zip(a, b):
+        product *= a ** b
+    return product
